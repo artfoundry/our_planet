09fe92de5e449e2a08e8bbb497bc85df4ed238ea
dbc-warrior <apprentice@devbootcamp.com>
2013-09-18 16:44:26 -0700

    add login with cookie and create unusable rspec test

--- app/controllers.rb
+++ app/controllers.rb
@@ -3,11 +3,21 @@ require 'sinatra'
 enable :sessions
 
 get '/' do
+  session['username'] ||= nil
   erb :home
 end
 
 get '/login' do
   erb :login
+end
+
+get '/home' do
+  erb :home
+end
+
+post '/login' do
+  session['username'] = params[:username]
+  redirect '/home'
 end
 
 helpers do 
--- login_spec.rb
+++ login_spec.rb
@@ -10,4 +10,14 @@ def app
   Sinatra::Application
 end
 
+enable :sessions
+
 set :database, "sqlite3:///db/members.db"
+
+describe 'login process' do
+
+  xit "should save the username to a cookie on login" do
+    post '/login', params={username:"James Bond"}
+    session['username'] == params[:username]
+  end
+end
