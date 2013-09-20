4cb339cc7866632d6eca8fc9560dda83799acfde
Bruno Sanches <brunopsanches@gmail.com>
2013-09-19 18:00:47 -0700

    Add production and devlopment environments

--- app/config.rb
+++ app/config.rb
@@ -8,5 +8,11 @@ require_relative 'controllers'
 if settings.test?
   set :database, "sqlite3:///db/members_test.db"
 else
-  set :database, "sqlite3:///db/members_development.db"
+  configure :development do
+    set :database, "sqlite3:///mydb.sqlite3"
+  end
+
+  configure :production do
+    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  end
 end
